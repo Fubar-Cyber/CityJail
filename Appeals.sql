9a7c18ff2193101b0d8509531cf19a915dcf282d
jensy <116689881+Fubar-Cyber@users.noreply.github.com>
2023-09-04 00:17:24 -0400

    NEWEST ALTER TABLE Appeals

--- Appeals.sql
+++ Appeals.sql
@@ -1,7 +1,2 @@
-CREATE TABLE Appeals (
-    Appeal_ID NUMERIC,
-    Crime_ID NUMERIC,
-    Filing_date DATE,
-    Hearing_date DATE,
-    Status CHAR(1) Default "P"
-);
+ALTER TABLE Appeals
+MODIFY COLUMN Status ENUM('P', 'A', 'D')  Default "P"
