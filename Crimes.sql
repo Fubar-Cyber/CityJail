62a2168999aeb6aba278fad881ef6136b961d99f
jensy <116689881+Fubar-Cyber@users.noreply.github.com>
2023-09-10 18:11:50 -0400

    add keys

--- Crimes.sql
+++ Crimes.sql
@@ -1,3 +1,7 @@
 ALTER TABLE Crimes
-MODIFY COLUMN Date_Recorded timestamp not null default now()
+ADD PRIMARY KEY (Crime_ID),
+ADD FOREIGN KEY (Criminal_ID) REFERENCES Criminals(Criminal_ID),
+MODIFY COLUMN Criminal_ID DECIMAL NOT NULL,
+ADD CONSTRAINT CHK_Classification CHECK (Classification IN ('F', 'M', 'O', 'U')),
+ADD CONSTRAINT CHK_Status CHECK (Status IN ('CL', 'CA', 'IA'))
 
