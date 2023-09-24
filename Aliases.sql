6fda05088d1aa1f74f3cff83f46290f53880ec0b
jensy <116689881+Fubar-Cyber@users.noreply.github.com>
2023-09-24 16:23:38 -0400

    D

--- Aliases.sql
+++ Aliases.sql
@@ -1,3 +1,6 @@
 ALTER TABLE Aliases
-MODIFY COLUMN Criminal_ID decimal NOT NULL;
+ADD COLUMN Criminal_ID INT,
+ADD CONSTRAINT FK_Criminal_Aliases
+FOREIGN KEY (Criminal_ID)
+REFERENCES Criminals(Criminal_ID);
 
