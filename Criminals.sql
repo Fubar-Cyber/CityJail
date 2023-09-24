3eab4f4d4fa1b5eb8f2ff1285966a56e2005d31a
jensy <116689881+Fubar-Cyber@users.noreply.github.com>
2023-09-24 17:42:10 -0400

    INDEXES

--- Criminals.sql
+++ Criminals.sql
@@ -1,2 +1,3 @@
-INSERT INTO criminals (Criminal_ID, Last, First, Street, City, Zip, Phone, V_status, P_status)
-VALUES (NULL, 'Capella', 'Manie', '1234 Any St', 'St Cloud', '34771', '4075555555', 'N', 'N');
+CREATE INDEX idx_last_name ON criminals (Last);
+CREATE INDEX idx_street ON criminals (Street);
+CREATE INDEX idx_phone ON criminals (Phone);
