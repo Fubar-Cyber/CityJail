d1ee97bf6bd5bcdf5a9fc7f09b4955a55ac2e6dd
jensy <116689881+Fubar-Cyber@users.noreply.github.com>
2023-10-13 23:01:46 -0400

    criminals update

--- Aliases.sql
+++ Aliases.sql
@@ -1,2 +1,2 @@
-INSERT INTO criminals (criminal_id, last, first, street, city, state, zip, phone, v_status, p_status)
-VALUES (1020, 'Phelps','Sam','1105 Tree Lane', 'Virginia Beach', 'VA', '23510', 7576778484, 'Y', 'N');
+ALTER TABLE Prob_officers
+ADD COLUMN mgr_id INT;
