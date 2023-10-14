62d94dedc0558b98398fbf8d80f0e53abe87e3fc
jensy <116689881+Fubar-Cyber@users.noreply.github.com>
2023-10-14 00:49:18 -0400

    CRIME OFFICER ADD

--- Crime_officers.sql
+++ Crime_officers.sql
@@ -1,4 +1,29 @@
+INSERT INTO crime_officers (crime_id, officer_id) 
+VALUES (10088, 111115); 
 
+INSERT INTO crime_officers (crime_id, officer_id)
+VALUES (10089, 111115); 
+
+INSERT INTO crime_officers (crime_id, officer_id)
+VALUES (10090, 111115); 
+
+INSERT INTO crime_officers (crime_id, officer_id) 
+VALUES (10091, 111115); 
+
+INSERT INTO crime_officers (crime_id, officer_id)
+VALUES (10092, 111115); 
+
+INSERT INTO crime_officers (crime_id, officer_id)
+VALUES (10093, 111115);
+
+INSERT INTO crime_officers (crime_id, officer_id)
+VALUES (10094, 111115);
+
+INSERT INTO crime_officers (crime_id, officer_id)
+VALUES (25344031, 111115);
+
+INSERT INTO crime_officers (crime_id, officer_id)
+VALUES (25344060, 111116);
 
 
 
